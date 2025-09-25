ebc79d3f78f90cb881cf44dc5b56014da4b0d16c
Sahil-Soni18 <sahils1810@gmail.com>
2025-09-25 16:59:55 +0530

    Featured Blogs API Added...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,43 @@
-import LoginPage from '@/features/Auth/components/Login';
-import SignupPage from '@/features/Auth/components/Signup';
-import FeaturedBlogs from '@/features/Home/components/FeaturedBlogs';
-import Hero from '@/features/Home/components/HeroSection';
-import { Typography, Box, Paper } from '@mui/material';
+import LoginPage from "@/features/Auth/components/Login";
+import SignupPage from "@/features/Auth/components/Signup";
+import FeaturedBlogs from "@/features/Home/components/FeaturedBlogs";
+import Hero from "@/features/Home/components/HeroSection";
+import { Typography, Box, Paper } from "@mui/material";
 
-export default function Home() {
+async function getFeaturedBlogs() {
+  console.log(`${process.env.NEXT_PUBLIC_API_BASE_URL}blog/featured`);
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}blog/featured`,
+      {
+        cache: "force-cache",
+        next: {
+          revalidate: 3600,
+          tags: ["blogs", "featured"],
+        },
+      }
+    );
+
+    console.log(`response: ${JSON.stringify(response)}`)
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch featured blogs: ${response.status}`);
+    }
+
+    const featuredBlogs = await response.json();
+    return featuredBlogs;
+  } catch (error) {
+    console.error("Error fetching blogs:", error);
+    return [];
+  }
+}
+export default async function Home() {
+
+  const featuredBlogs = await getFeaturedBlogs();
   return (
     <Box>
       <Hero />
-      <FeaturedBlogs />
+      <FeaturedBlogs featuredBlogs={featuredBlogs} />
       {/* <SignupPage /> */}
     </Box>
   );
--- src/features/Home/components/FeaturedBlogs/index.tsx
+++ src/features/Home/components/FeaturedBlogs/index.tsx
@@ -1,14 +1,15 @@
 'use client'
 
 import BlogCard from "@/features/Blogs/components/BlogCard";
-import { Stack, Typography, Box } from "@mui/material";
+import { IBlog } from "@/features/Blogs/types";
+import { Stack, Typography, Box, Grid } from "@mui/material";
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 interface FeaturedBlogsProps {
-  // Add any props you might need in the future
+  featuredBlogs: IBlog[]
 }
 
-const FeaturedBlogs: React.FC<FeaturedBlogsProps> = () => {
+const FeaturedBlogs: React.FC<FeaturedBlogsProps> = ({ featuredBlogs }) => {
   return (
     <Box sx={{ py: 6, px: { xs: 2, md: 4 } }}>
       <Typography 
@@ -23,26 +24,33 @@ const FeaturedBlogs: React.FC<FeaturedBlogsProps> = () => {
       >
         Featured Blogs
       </Typography>
-      <Stack 
-        spacing={4}
-        sx={{
-          maxWidth: '1200px',
-          mx: 'auto'
-        }}
+      <Grid 
+        container
+        spacing={3}
+        // sx={{
+        //   maxWidth: '1200px',
+        //   mx: 'auto'
+        // }}
       >
-        <BlogCard
-          imageUrl="https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
-          title="The Future of Web Development in 2023"
-          slug="the-future-of-web-development-in-2023"
-          description="Explore the latest trends and technologies that are shaping the future of web development. From AI-powered interfaces to serverless architectures, discover what's next in the world of coding."
-          created_at="June 15, 2023"
-          author={ {id: 1, name: "Sahil"}}
-          content="Some Content"
-          author_avatar="https://randomuser.me/api/portraits/women/43.jpg"
-          technologies={["Technology"]}
-          read_time={7}
-        />
-      </Stack>
+        {featuredBlogs.map((blog: IBlog, index: number ) => (
+          <Grid size={{ xs: 12, sm: 6, md: 4 }} key={blog.id || index }>
+            <BlogCard
+              id={blog.id}
+              imageUrl={blog.imageUrl || ""}
+              slug={blog.slug}
+              title={blog.title}
+              content={blog.content}
+              description={blog.description}
+              created_at={blog.created_at}
+              author={blog.author}
+              author_avatar={blog.author_avatar}
+              technologies={blog.technologies}
+              read_time={blog.read_time}
+            />
+          </Grid>
+        ))}
+       
+      </Grid>
     </Box>
   );
 };
